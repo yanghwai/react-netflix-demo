00c34b9bb2a8c52b3be981478f8f31418335257e
Huai Yang <huaiyang@microsoft.com>
2020-11-30 16:49:43 +0800

    --feat: add jumbotrons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import Jumbotron from "./components/jumbotron";
+
+import jumboData from "./fixtures/jumbotron.json";
+
 function App() {
   return (
-    <p>Hello</p>
+    <Jumbotron.Container>
+      {jumboData.map((item, index) => {
+        const itemDirection = index % 2 === 0 ? "row" : "row-reverse";
+        return (
+          <Jumbotron key={item.id} direction={itemDirection}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        );
+      })}
+    </Jumbotron.Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { GlobalStyles } from "./global-styles";
+import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
